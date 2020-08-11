37bad0bf69cd37dabac1b75a9f2a37247692e7cb
Carolain <Carolainsilva1@gmail.com>
2020-08-11 11:52:36 -0300

    cree componente maintitle

---
+++ pricing-page/src/Components/MainTitle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//creo mi componente
+ class MainTitle extends React.Component{
+     render(){
+         return (
+             <div>
+                 <h2 class="Title">Pricing</h2>
+                 <div class="Description-container">
+                    <p1 class="Descriptions">Quickly build an effective pricing table for your potential<br></br> customers with this layout. It's built with default <br></br> Material-UI components with little customization.</p1>
+                 </div>
+             </div>
+         );
+     }
+ }
+
+ export default MainTitle;
